bd92493e147c4173692c8e43d9760b6c88a7f463
Leon Augustine <Laugusti@mail.usf.edu>
2019-07-30 14:46:25 -0400

    updating documentation

--- restclient/sobject.go
+++ restclient/sobject.go
@@ -1,14 +1,42 @@
 package restclient
 
 import (
-	"errors"
 	"fmt"
 	"strings"
 )
 
 var (
-	errInvalidField = errors.New("field name is required")
+	errInvalidField = &InvalidFieldErr{}
 )
+
+// InvalidFieldErr represents an invalid field error.
+type InvalidFieldErr struct {
+	FieldName string
+}
+
+func (e *InvalidFieldErr) Error() string {
+	return "field name is required"
+}
+
+// FieldNotFoundErr represents an error for field not in the object.
+type FieldNotFoundErr struct {
+	FieldName string
+}
+
+func (e *FieldNotFoundErr) Error() string {
+	return fmt.Sprintf("field %q not in salesforce object", e.FieldName)
+}
+
+// InvalidFieldTypeErr represents an invalid field type when traversing the object.
+type InvalidFieldTypeErr struct {
+	FullFieldName   string
+	FailedFieldName string
+	FailedField     interface{}
+}
+
+func (e *InvalidFieldTypeErr) Error() string {
+	return fmt.Sprintf("failed to get field %q at %q: want map got %T", e.FullFieldName, e.FailedFieldName, e.FailedField)
+}
 
 // SObjectField is represents a field on a Salesforce Object. This is used by the
 // Salesforce Object Builder to add fields to the Salesforce Object.
@@ -65,7 +93,12 @@ func (sb *SObjectBuilder) Build() SObject {
 // SObject stores a Salesforce Object used by the Salesforce API.
 type SObject map[string]interface{}
 
-// AddField adds the field to the sobject.
+// AddField adds the field to the Salesforce Object overwritting any existing value.
+// This methods returns an error if the field is invalid
+// Example:
+// sobj := SObject{}
+// _ = soject.AddField("a.b.c", 100)
+// The result of this is the following map: map[a.b.c:100]
 func (s SObject) AddField(field string, v interface{}) error {
 	if field == "" {
 		return errInvalidField
@@ -80,9 +113,21 @@ func (s SObject) mandatoryAddField(field string, v interface{}) {
 	}
 }
 
-// AddDottedField adds the field to the Salesforce Object.
-// The difference from AddField is this will traverse down the
-// object (creating paths if necessary) to add the field.
+// AddDottedField adds the field to the Salesforce Object overwritting any existing value.
+// The difference from AddField is this method will traverse down the object
+// (creating paths if necessary) to add the field.
+// This method returns an error if the field path is invalid or a non-leaf node in the field
+// path is set to a non map[string]interface{} value.
+// Example:
+// sobj := SObject{}
+// _ = soject.AddDottedField("a.b.c", 100)
+// The result of this is the following map: map[a:map[b:map[c:100]]]
+// Note: the following would return an error if attempted on the SObject above:
+// sobject.AddDottedField("a.b.c.d", 100)
+// This is because the `c` entry in the object is set to a non-map value a map .
+// The following does succeed however:
+// _ = sobject.AddDottedField("a.b.c", SObject{})
+// _ = sobject.AddDottedField("a.b.c.d", 100)
 func (s SObject) AddDottedField(dottedField string, v interface{}) error {
 	sobj, err := getLastSObjectInPath(s, dottedField, true, false)
 	if err != nil {
@@ -95,21 +140,32 @@ func (s SObject) AddDottedField(dottedField string, v interface{}) error {
 }
 
 // GetField returns the field value on the Salesforce Object.
+// This returns an error if the field is invalid or the field is not in the object.
+// Example:
+// sobj := NewSObjectBuilder().AddDottedField("a.b.c", 100).AddField("a.b.c", 200).Build()
+// v, _ = s.GetField("a.b.c")
+// The value of v will be 200
+// Refer to AddField and AddDottedField.
 func (s SObject) GetField(field string) (interface{}, error) {
 	if field == "" {
 		return nil, errInvalidField
 	}
 	v, ok := s[field]
 	if !ok {
-		return nil, fmt.Errorf("field %q not in salesforce object", field)
+		return nil, &FieldNotFoundErr{field}
 	}
 	return v, nil
 }
 
 // GetDottedField returns the field value on the Salesforce Object.
-// The difference from GetField is this will traverse down the
-// object to retrieve the field.
-// Example s.GetDottedField("Contact.Name"
+// The difference from GetField is this will traverse down the object to retrieve the field.
+// This methoed returns error if the field is invalid, a non-leaf node is a non-map value, or the
+// field is not in the object.
+// Example:
+// sobj := NewSObjectBuilder().AddDottedField("a.b.c", 100).AddField("a.b.c", 200).Build()
+// v, _ = s.GetField("a.b.c")
+// The value of v will be 100
+// Refer to AddField and AddDottedField.
 func (s SObject) GetDottedField(dottedField string) (interface{}, error) {
 	sobj, err := getLastSObjectInPath(s, dottedField, false, false)
 	if err != nil {
@@ -118,10 +174,20 @@ func (s SObject) GetDottedField(dottedField string) (interface{}, error) {
 	return sobj.GetField(getLastFieldInPath(dottedField))
 }
 
-// GetMandatoryField ensures the field is valid for the Salesforce Object and there was no error
-// when calling the GetField function.
+// GetMandatoryField calls GetField and panic if it receives an error.
+// Refer to the GetField documentation.
 func (s SObject) GetMandatoryField(field string) interface{} {
 	v, err := s.GetField(field)
+	if err != nil {
+		panic(err)
+	}
+	return v
+}
+
+// GetMandatoryDottedField calls GetDottedField and panic if it receives an error.
+// Refer to the GetDottedField documentation.
+func (s SObject) GetMandatoryDottedField(dottedField string) interface{} {
+	v, err := s.GetDottedField(dottedField)
 	if err != nil {
 		panic(err)
 	}
@@ -158,8 +224,7 @@ func getLastSObjectInPath(s SObject, dottedField string, createIfNotExists, repl
 		} else {
 			// found field, value must be map
 			if _, ok := v.(map[string]interface{}); !ok && !replaceIfWrongType {
-				return nil, fmt.Errorf("failed to get field %q: want map got %T",
-					dottedField, v)
+				return nil, &InvalidFieldTypeErr{dottedField, field, v}
 			} else if !ok && replaceIfWrongType {
 				sobj.mandatoryAddField(field, map[string]interface{}{})
 			}
