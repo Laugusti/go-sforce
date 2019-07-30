0a95e092508818ec51e8bda74701df10e02963fe
Leon Augustine <Laugusti@mail.usf.edu>
2019-07-29 21:48:17 -0400

    refactoring unit test

--- session/session_test.go
+++ session/session_test.go
@@ -2,14 +2,12 @@ package session
 
 import (
 	"fmt"
-	"io"
 	"net/http"
-	"net/http/httptest"
 	"net/url"
-	"strings"
 	"testing"
 
 	"github.com/Laugusti/go-sforce/credentials"
+	"github.com/Laugusti/go-sforce/internal/testserver"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -58,43 +56,119 @@ func TestNew(t *testing.T) {
 }
 
 func TestLogin(t *testing.T) {
-	s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		err := r.ParseForm()
-		assert.Nil(t, err)
-		checkLoginFormValues(t, r.Form, "grant_type", "password")
-		checkLoginFormValues(t, r.Form, "client_id", "clientid")
-		checkLoginFormValues(t, r.Form, "client_secret", "clientsecret")
-		checkLoginFormValues(t, r.Form, "username", "user")
-		checkLoginFormValues(t, r.Form, "password", "pass")
-		_, err = io.Copy(w, strings.NewReader(`{"instance_url": "testUrl", "access_token": "testToken"}`))
-		assert.Nil(t, err)
-	}))
-	defer s.Close()
-	sess := &Session{
-		LoginURL: s.URL,
-		creds: &credentials.OAuth{
-			Username:     "user",
-			Password:     "pass",
-			ClientID:     "clientid",
-			ClientSecret: "clientsecret",
-		},
-		httpClient: s.Client(),
+	server := testserver.New(t)
+	defer server.Stop()
+
+	tests := []struct {
+		// inputs
+		shouldErr    bool
+		apiVersion   string
+		username     string
+		password     string
+		clientID     string
+		clientSecret string
+		// wants
+		accessToken string
+		instanceURL string
+		id          string
+		tokenType   string
+		issuedAt    string
+		signature   string
+	}{
+		{false, "1.0", "user", "pass", "id", "secret", "", "", "", "", "", ""},
+		{false, "2.0", "u", "p", "i", "s", "token", "url", "id", "type", "at", "sig"},
+		{true, "1.0", "user", "pass", "id", "secret", "", "", "", "", "", ""},
 	}
 
-	if sess.HasToken() {
-		t.Error("TestLogin => should not have token")
-	}
-	err := sess.Login()
-	assert.Nil(t, err)
+	for _, test := range tests {
+		assertMsg := fmt.Sprintf("input: %v", test)
 
-	assert.True(t, sess.HasToken(), "should have token")
-	assert.Equal(t, "testToken", sess.AccessToken(), "wrong access token")
-	assert.Equal(t, "testUrl", sess.InstanceURL(), "wrong instance url")
+		// wanted request token
+		wantToken := RequestToken{
+			AccessToken: test.accessToken,
+			InstanceURL: test.instanceURL,
+			ID:          test.id,
+			TokenType:   test.tokenType,
+			IssuedAt:    test.issuedAt,
+			Signature:   test.signature,
+		}
+
+		// oauth creds
+		creds := credentials.New(
+			test.username,
+			test.password,
+			test.clientID,
+			test.clientSecret,
+		)
+
+		// set server handler func
+		setHandlerFunc(t, assertMsg, server, *creds, wantToken, test.shouldErr)
+
+		// create new session
+		sess := Must(New(server.URL(), test.apiVersion, creds))
+		sess.httpClient = server.Client()
+
+		// assert session does not have token
+		assert.Nil(t, sess.requestToken, assertMsg)
+
+		// set dummy request token to ensure it is overwritten
+		sess.requestToken = &RequestToken{"DUMMY", "DUMMY", "DUMMY", "DUMMY", "DUMMY", "DUMMY"}
+
+		// try session login
+		err := sess.Login()
+
+		// session assertions
+		if test.shouldErr {
+			assert.NotNil(t, err, assertMsg)
+			assert.Nil(t, sess.requestToken, assertMsg)
+		} else {
+			assert.Nil(t, err, assertMsg)
+			assert.NotNil(t, sess.requestToken, assertMsg)
+			assert.True(t, sess.HasToken(), assertMsg)
+			assert.Equal(t, &wantToken, sess.requestToken, assertMsg)
+			assert.Equal(t, test.instanceURL, sess.InstanceURL(), assertMsg)
+			assert.Equal(t, test.accessToken, sess.AccessToken(), assertMsg)
+		}
+	}
 }
 
-func checkLoginFormValues(t *testing.T, form url.Values, key, want string) {
-	got := form.Get(key)
-	if got != want {
-		t.Errorf("TestLogin => wrong %s: expected %s, got %s", key, want, got)
+func setHandlerFunc(t *testing.T, assertMsg string, s *testserver.Server, creds credentials.OAuth,
+	token RequestToken, shouldErr bool) {
+	// default handler returns token and 200 status
+	handler := &testserver.JSONResponseHandler{
+		StatusCode: http.StatusOK,
+		Body:       token,
 	}
+	// error handler returns error response and 400 status
+	if shouldErr {
+		handler = &testserver.JSONResponseHandler{
+			StatusCode: http.StatusBadRequest,
+			Body: map[string]interface{}{
+				"error":             "login_failed",
+				"error_description": "authentication failure",
+			},
+		}
+	}
+	// validating query={empty}, method=POST,
+	// path=oathtokenpath, form=creds, body={empty}
+	validators := []testserver.RequestValidator{
+		&testserver.QueryValidator{Query: url.Values{}},
+		&testserver.MethodValidator{Method: http.MethodPost},
+		&testserver.PathValidator{Path: "/services/oauth2/token"},
+		&testserver.FormValidator{Form: credsAsForm(creds)},
+		&testserver.JSONBodyValidator{}, // validate body after form
+	}
+
+	// set server handler func
+	s.HandlerFunc = testserver.ValidateRequestHandlerFunc(t, assertMsg, handler, validators...)
+}
+
+func credsAsForm(creds credentials.OAuth) url.Values {
+	form := url.Values{}
+	form.Set("grant_type", "password")
+	form.Set("username", creds.Username)
+	form.Set("password", creds.Password)
+	form.Set("client_id", creds.ClientID)
+	form.Set("client_secret", creds.ClientSecret)
+	return form
 }
