54dbf73ea0723dfe8123bcd41bb66893af520033
leon <laugusti@mail.usf.edu>
2019-08-02 16:30:38 -0400

    formatting with go-fmt

--- cmd/sforce/main.go
+++ cmd/sforce/main.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2019 
+Copyright © 2019
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -18,5 +18,5 @@ package main
 import "github.com/Laugusti/go-sforce/cmd/sforce/cmd"
 
 func main() {
-  cmd.Execute()
+	cmd.Execute()
 }
