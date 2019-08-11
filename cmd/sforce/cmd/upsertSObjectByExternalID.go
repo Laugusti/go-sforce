818b4bf7d27878b4e4b54796b23f179be6defe58
leon <laugusti@mail.usf.edu>
2019-08-11 00:09:40 -0400

    adding support for UpsertSObjectByExternalID to cli

---
+++ cmd/sforce/cmd/upsertSObjectByExternalID.go
@@ -0,0 +1,45 @@
+package cmd
+
+import (
+	restapi "github.com/Laugusti/go-sforce/api/rest"
+	"github.com/spf13/cobra"
+)
+
+// upsertSObjectByExternalIDCmd represents the upsertSObjectByExternalID command
+var upsertSObjectByExternalIDCmd = &cobra.Command{
+	Use:   "upsertSObjectByExternalID <name> <extidfield> <extid> [<file>]",
+	Args:  cobra.RangeArgs(3, 4),
+	Short: "Create/Update an existing SObject using the Object Name, External ID Field, External ID and data file",
+	Long: `Create/Update an existing SObject using the Object Name, External ID Field, External ID and data file.
+With no file or when file is -, read standard input`,
+	Run: func(cmd *cobra.Command, args []string) {
+		// get file argument or use -
+		file := "-"
+		if len(args) == 4 {
+			file = args[3]
+		}
+
+		// unmarshal file to sobject
+		var sobj restapi.SObject
+		unmarshalJSONFile("UpsertSObjectByExternalID", file, &sobj)
+
+		// create api input
+		input := &restapi.UpsertSObjectByExternalIDInput{
+			SObjectName:     args[0],
+			ExternalID:      args[1],
+			ExternalIDField: args[2],
+			SObject:         sobj,
+		}
+
+		// do api request
+		out, err := restClient.UpsertSObjectByExternalID(input)
+		exitIfError("UpsertSObjectByExternalIDInput", err)
+
+		// write result to stdout
+		marshalJSONToStdout("UpsertSObjectByExternalIDInput", out.Result)
+	},
+}
+
+func init() {
+	restCmd.AddCommand(upsertSObjectByExternalIDCmd)
+}
